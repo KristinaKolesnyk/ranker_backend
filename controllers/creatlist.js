9326e30c3a3e4c7dd5124b2af791f857fddf202f
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2024-08-03 19:09:09 +0300

    new createlist

--- controllers/creatlist.js
+++ controllers/creatlist.js
@@ -1,43 +1,89 @@
 const handleCreatList = (req, res, db) => {
-    const {categoryName, criteriaName, userId} = req.body;
+    const { categoryName, criteriaName, userId } = req.body;
 
     db.transaction(trx => {
-        trx.insert({
-            name: categoryName
-        })
-            .into('category')
-            .returning('id')
-            .then(categoryId => {
-                const category_id = categoryId[0].id
+        // Проверка, существует ли коллекция для данного пользователя
+        trx('collection')
+            .where({ user_id: userId })
+            .first()
+            .then(existingCollection => {
+                if (existingCollection) {
+                    // Коллекция существует, используем её collection_id
+                    const collection_id = existingCollection.id;
 
-                return trx('collection')
-                    .returning('*')
-                    .insert({
-                        category_id,
-                        user_id: userId
-                    })
-                    .then(collection => {
-                        const criteriaInsertPromises = criteriaName.map(criterion =>
-                            trx('criterion')
-                                .returning('*')
+                    return trx('category')
+                        .returning('id')
+                        .insert({
+                            name: categoryName,
+                            collection_id: collection_id
+                        })
+                        .then(categoryId => {
+                            const category_id = categoryId[0].id;
+
+                            const criteriaInsertPromises = criteriaName.map(criterion =>
+                                trx('criterion')
+                                    .returning('*')
+                                    .insert({
+                                        name: criterion,
+                                        category_id
+                                    })
+                            );
+
+                            return Promise.all(criteriaInsertPromises)
+                                .then(criteria => {
+                                    res.json({
+                                        collection: existingCollection,
+                                        category: { id: category_id, name: categoryName },
+                                        criteria: criteria.map(criterion => criterion[0])
+                                    });
+                                });
+                        });
+                } else {
+                    // Коллекция не существует, создаем новую коллекцию
+                    return trx('collection')
+                        .returning('id')
+                        .insert({
+                            user_id: userId
+                        })
+                        .then(collectionId => {
+                            const collection_id = collectionId[0].id;
+
+                            return trx('category')
+                                .returning('id')
                                 .insert({
-                                    name: criterion,
-                                    category_id
+                                    name: categoryName,
+                                    collection_id: collection_id
                                 })
-                        )
-                        return Promise.all(criteriaInsertPromises)
-                            .then(criteria => {
-                                res.json({
-                                    collection: collection[0],
-                                    criterion: criteria.map(criterion => criterion[0])
-                                })
-                            })
-                    })
+                                .then(categoryId => {
+                                    const category_id = categoryId[0].id;
+
+                                    const criteriaInsertPromises = criteriaName.map(criterion =>
+                                        trx('criterion')
+                                            .returning('*')
+                                            .insert({
+                                                name: criterion,
+                                                category_id
+                                            })
+                                    );
+
+                                    return Promise.all(criteriaInsertPromises)
+                                        .then(criteria => {
+                                            res.json({
+                                                collection: { id: collection_id },
+                                                category: { id: category_id, name: categoryName },
+                                                criteria: criteria.map(criterion => criterion[0])
+                                            });
+                                        });
+                                });
+                        });
+                }
             })
             .then(trx.commit)
-            .catch(trx.rollback)
-    }).catch(err => res.status(400).json('Unable to register'))
-}
+            .catch(trx.rollback);
+    }).catch(err => res.status(400).json('Unable to create list'));
+};
+
+
 
 module.exports = {
     handleCreatList: handleCreatList
