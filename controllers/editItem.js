fc77066ba3964883259c0fbe3968626a5d2fe25c
Kristina <kkristina.kolesnyk@gmail.com>
2024-08-06 22:43:02 +0300

    from edit item to your list

--- controllers/editItem.js
+++ controllers/editItem.js
@@ -1,7 +1,13 @@
-const {calculateAverageRating} = require('../controllers/utils')
-
 const handleEditItem = (req, res, db) => {
     const {id, url, ratings, criteria} = req.body;
+
+    const calculateAverageRating = (ratings) => {
+        const numericRatings = ratings.map(r => parseFloat(r)).filter(r => !isNaN(r));
+        const sum = numericRatings.reduce((acc, val) => acc + val, 0);
+        const average = numericRatings.length > 0 ? sum / numericRatings.length : 0;
+        return parseFloat(average.toFixed(1));
+    }
+
     const avgRating = calculateAverageRating(ratings);
 
     db.transaction(trx => {
